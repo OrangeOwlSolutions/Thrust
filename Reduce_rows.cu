cb1f9b7a13760a9545e922869c925f840ee73361
Orange Owl Solutions <orangeowlsolutions@gmail.com>
2015-04-14 07:14:39 +0200

    Update Reduce_rows.cu

--- Reduce_rows.cu
+++ Reduce_rows.cu
@@ -47,6 +47,17 @@ struct row_reduction {
 	}
 };
 
+/**************************/
+/* NEEDED FOR APPROACH #3 */
+/**************************/
+template<typename T>
+struct MulC: public thrust::unary_function<T, T>
+{
+    T C;
+    __host__ __device__ MulC(T c) : C(c) { }
+    __host__ __device__ T operator()(T x) { return x * C; }
+};
+
 /********/
 /* MAIN */
 /********/
@@ -74,14 +85,21 @@ int main()
 	thrust::device_vector<int> d_row_indices(Nrows);
   
 	// --- Compute row sums by summing values with equal row indices
-	thrust::reduce_by_key(thrust::make_transform_iterator(thrust::counting_iterator<int>(0), linear_index_to_row_index<int>(Ncols)),
-						  thrust::make_transform_iterator(thrust::counting_iterator<int>(0), linear_index_to_row_index<int>(Ncols)) + (Nrows*Ncols),
-						  d_matrix.begin(),
-						  d_row_indices.begin(),
-						  d_row_sums.begin(),
-						  thrust::equal_to<int>(),
-						  thrust::plus<float>());
+	//thrust::reduce_by_key(thrust::make_transform_iterator(thrust::counting_iterator<int>(0), linear_index_to_row_index<int>(Ncols)),
+	//					  thrust::make_transform_iterator(thrust::counting_iterator<int>(0), linear_index_to_row_index<int>(Ncols)) + (Nrows*Ncols),
+	//					  d_matrix.begin(),
+	//					  d_row_indices.begin(),
+	//					  d_row_sums.begin(),
+	//					  thrust::equal_to<int>(),
+	//					  thrust::plus<float>());
 
+	thrust::reduce_by_key(
+                thrust::make_transform_iterator(thrust::make_counting_iterator(0), linear_index_to_row_index<int>(Ncols)),
+                thrust::make_transform_iterator(thrust::make_counting_iterator(0), linear_index_to_row_index<int>(Ncols)) + (Nrows*Ncols),
+                d_matrix.begin(),
+                thrust::make_discard_iterator(),
+                d_row_sums.begin());
+	
 	printf("Timing for approach #1 = %f\n", timerGPU.GetCounter());
 	
 	// --- Print result
@@ -113,19 +131,24 @@ int main()
 	/***************/
 	/* APPROACH #3 */
 	/***************/
-	cublasHandle_t handle;
 
 	timerGPU.StartCounter();
-	cublasSafeCall(cublasCreate(&handle));
-
-	thrust::device_vector<float> d_row_sums_3(Nrows);
-	thrust::device_vector<float> d_ones(Ncols, 1.f);
-
-	float alpha = 1.f;
-	float beta  = 0.f;
-	cublasSafeCall(cublasSgemv(handle, CUBLAS_OP_T, Ncols, Nrows, &alpha, thrust::raw_pointer_cast(d_matrix.data()), Ncols, 
-		                       thrust::raw_pointer_cast(d_ones.data()), 1, &beta, thrust::raw_pointer_cast(d_row_sums_3.data()), 1));
-
+	thrust::device_vector<float> d_row_sums_3(Nrows, 0);
+	thrust::device_vector<float> d_temp(Nrows * Ncols);
+	thrust::inclusive_scan_by_key(
+                thrust::make_transform_iterator(thrust::make_counting_iterator(0), linear_index_to_row_index<int>(Ncols)),
+                thrust::make_transform_iterator(thrust::make_counting_iterator(0), linear_index_to_row_index<int>(Ncols)) + (Nrows*Ncols),
+                d_matrix.begin(),
+                d_temp.begin());
+    thrust::copy(
+                thrust::make_permutation_iterator(
+                        d_temp.begin() + Ncols - 1,
+                        thrust::make_transform_iterator(thrust::make_counting_iterator(0), MulC<int>(Ncols))),
+    thrust::make_permutation_iterator(
+                        d_temp.begin() + Ncols - 1,
+                        thrust::make_transform_iterator(thrust::make_counting_iterator(0), MulC<int>(Ncols))) + Ncols,
+                d_row_sums_3.begin());
+		
 	printf("Timing for approach #3 = %f\n", timerGPU.GetCounter());
 
 	for(int i = 0; i < Nrows; i++) {
@@ -135,6 +158,30 @@ int main()
 		std::cout << "] = " << d_row_sums_3[i] << "\n";
 	}
 
+	/***************/
+	/* APPROACH #4 */
+	/***************/
+	cublasHandle_t handle;
+
+	timerGPU.StartCounter();
+	cublasSafeCall(cublasCreate(&handle));
+
+	thrust::device_vector<float> d_row_sums_4(Nrows);
+	thrust::device_vector<float> d_ones(Ncols, 1.f);
+
+	float alpha = 1.f;
+	float beta  = 0.f;
+	cublasSafeCall(cublasSgemv(handle, CUBLAS_OP_T, Ncols, Nrows, &alpha, thrust::raw_pointer_cast(d_matrix.data()), Ncols, 
+		                       thrust::raw_pointer_cast(d_ones.data()), 1, &beta, thrust::raw_pointer_cast(d_row_sums_4.data()), 1));
+
+	printf("Timing for approach #4 = %f\n", timerGPU.GetCounter());
+
+	for(int i = 0; i < Nrows; i++) {
+		std::cout << "[ ";
+		for(int j = 0; j < Ncols; j++)
+			std::cout << d_matrix[i * Ncols + j] << " ";
+		std::cout << "] = " << d_row_sums_4[i] << "\n";
+	}
+
 	return 0;
 }
-
