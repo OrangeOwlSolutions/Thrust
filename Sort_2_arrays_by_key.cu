137de541e1458e174e0292c19aa7782c0d6c4a84
Orange Owl <orangeowlsolutions@gmail.com>
2017-02-27 12:37:28 +0100

    Create Sort_2_arrays_by_key.cu

---
+++ Sort_2_arrays_by_key.cu
@@ -0,0 +1,73 @@
+#include <time.h>       // --- time
+#include <stdlib.h>     // --- srand, rand
+
+#include <thrust\host_vector.h>
+#include <thrust\device_vector.h>
+#include <thrust\sort.h>
+#include <thrust\iterator\zip_iterator.h>
+
+#include "TimingGPU.cuh"
+
+//#define VERBOSE
+#define COMPACT
+
+int main() {
+
+	const int N = 131072;
+	//const int N = 10;
+
+	TimingGPU timerGPU;
+	
+	// --- Initialize random seed
+	srand(time(NULL));
+
+	thrust::host_vector<int> h_code(N);
+	thrust::host_vector<double> h_x(N);
+	thrust::host_vector<double> h_y(N);
+
+	for (int k = 0; k < N; k++) {		
+		// --- Generate random numbers between 0 and 9
+		h_code[k] = rand() % 10 + 1;
+		h_x[k] = ((double)rand() / (RAND_MAX));
+		h_y[k] = ((double)rand() / (RAND_MAX));
+	}
+
+	thrust::device_vector<int> d_code(h_code);
+
+	thrust::device_vector<double> d_x(h_x);
+	thrust::device_vector<double> d_y(h_y);
+
+#ifdef VERBOSE
+	printf("Before\n");
+	for (int k = 0; k < N; k++) printf("code = %i; x = %f; y = %f\n", h_code[k], h_x[k], h_y[k]);
+#endif
+
+	timerGPU.StartCounter();
+#ifdef COMPACT
+	thrust::sort_by_key(d_code.begin(), d_code.end(), thrust::make_zip_iterator(thrust::make_tuple(d_x.begin(), d_y.begin())));
+#else
+	
+	// --- Initialize indices vector to [0,1,2,..]
+	thrust::counting_iterator<int> iter(0);
+	thrust::device_vector<int> indices(N);
+	thrust::copy(iter, iter + indices.size(), indices.begin());
+
+	// --- First, sort the keys and indices by the keys
+	thrust::sort_by_key(d_code.begin(), d_code.end(), indices.begin());
+
+	// Now reorder the ID arrays using the sorted indices
+	thrust::gather(indices.begin(), indices.end(), d_x.begin(), d_x.begin());
+	thrust::gather(indices.begin(), indices.end(), d_y.begin(), d_y.begin());
+#endif
+
+	printf("Timing GPU = %f\n", timerGPU.GetCounter());
+
+#ifdef VERBOSE
+	h_code = d_code;
+	h_x = d_x;
+	h_y = d_y;
+	
+	printf("After\n");
+	for (int k = 0; k < N; k++) printf("code = %i; x = %f; y = %f\n", h_code[k], h_x[k], h_y[k]);
+#endif
+}
